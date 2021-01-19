86a0ff1ccb11ce111488348b9a8ba3048024b387
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-01-19 15:34:32 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -15,7 +15,7 @@ Join for List of Telegram Bots : InlineKeyboardButton(text, url = https://t.me/T
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
     NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
-    DOWNLOAD_START = " 📥Downloading📥 Wait untill it completed."
+    DOWNLOAD_START = " 📥Downloading📥         Wait untill it completed."
     UPLOAD_START = " 📤Uploading📤 "
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
@@ -45,7 +45,7 @@ Telegram ID: <code>{}</code>
    Video  - Give File as video without Screenshots
    DFile  - Give File without Screenshots
 
-Support Group : https://t.me/TGBotsCollection"""
+Join : https://t.me/TGBotsCollection"""
     REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media to get High Speed Direct Download Link"
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
     REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
